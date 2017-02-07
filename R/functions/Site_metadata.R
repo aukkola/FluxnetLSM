2e9a10fa5620fb2d56a797f2a5f597fbe8c89143
naught101 <naught101@naught101.org>
2017-02-07 11:46:19 +1100

    get Multiple ORNL URLs at once

--- R/functions/Site_metadata.R
+++ R/functions/Site_metadata.R
@@ -108,23 +108,33 @@ get_ornl_site_codes <- function() {
 }
 
 
-#' Get ORNL site URL from site_status table
+#' Get a single ORNL site URL from site_status table
 get_site_ornl_url <- function(site_code) {
+    ornl_url <- get_site_ornl_urls(list(site_code))[[site_code]]
+    return(ornl_url)
+}
+
+
+#' Get a list of ORNL site URLs from site_status table
+get_site_ornl_urls <- function(site_code_list) {
     status_table_url <- "https://fluxnet.ornl.gov/site_status"
 
     page_html <- read_html(status_table_url)
 
-    # looks for table cell with site code as contents, then looks up the parent
-    # row, and finds the href of the first link.
-    xpath =  paste0("//td[text()='", site_code, "']/..")
-    ornl_rel_url <- page_html %>% html_node(xpath = xpath) %>%
-        html_node("a") %>%
-        html_attr("href")
+    ornl_url_list <- list()
 
-    ornl_url <- paste0("https://fluxnet.ornl.gov/", ornl_rel_url)
+    for (sc in site_code_list) {
+        # looks for table cell with site code as contents, then looks up the parent
+        # row, and finds the href of the first link.
+        xpath =  paste0("//td[text()='", site_code, "']/..")
+        ornl_rel_url <- page_html %>% html_node(xpath = xpath) %>%
+            html_node("a") %>%
+            html_attr("href")
 
-    return(ornl_url)
+        ornl_url_list[[sc]] <- paste0("https://fluxnet.ornl.gov/", ornl_rel_url)
+    }
 
+    return(ornl_url_list)
 }
 
 
