4397b9c2182b2b5391968272c994ab096bcb2d34
naught101 <naught101@naught101.org>
2017-02-08 10:10:43 +1100

    move Site_metadata.R to R/

--- tests/ConvertSpreadsheetToNcdf_AU-How.R
+++ tests/ConvertSpreadsheetToNcdf_AU-How.R
@@ -1,9 +1,9 @@
 # TODO: update when converted to package
-source("./R/functions/ConvertSpreadsheetToNcdf.R ")
+source("./R/ConvertSpreadsheetToNcdf.R")
 
 #Example call
 convert_fluxnet_to_netcdf(infile="~/Documents/FLUXNET2016_processing/FLX_AU-How_FLUXNET2015_FULLSET_HH_2001-2014_1-3.csv",
-                          site_name="AU-How",
+                          site_code="AU-How",
                           out_path="~/Documents/FLUXNET2016_processing/",
                           lib_path="~/Documents/FLUXNET2016_processing/scripts/R",    #TEMPORARY, remove when code turned into a package
                           era_file="~/Documents/FLUXNET2016_processing/FLX_AU-How_FLUXNET2015_ERAI_HH_1989-2014_1-3.csv",
