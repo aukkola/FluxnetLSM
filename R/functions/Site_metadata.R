426c2c7527c7db0e4c922b0caf106095774ae176
naught101 <naught101@naught101.org>
2017-02-07 13:28:28 +1100

    Rename ORNL functions, skip missing table

--- R/functions/Site_metadata.R
+++ R/functions/Site_metadata.R
@@ -102,7 +102,7 @@ get_ornl_site_codes <- function() {
 
     table <- page_html %>% html_node("#historical_site_list") %>% html_table()
 
-    site_codes = table["FLUXNET ID"]
+    site_codes <- table[["FLUXNET ID"]]
 
     return(site_codes)
 
@@ -111,20 +111,20 @@ get_ornl_site_codes <- function() {
 
 #' Get a single ORNL site URL from site_status table
 get_site_ornl_url <- function(site_code) {
-    ornl_url <- get_site_ornl_urls(list(site_code))[[site_code]]
+    ornl_url <- get_ornl_site_url_list(list(site_code))[[site_code]]
     return(ornl_url)
 }
 
 
 #' Get a list of ORNL site URLs from site_status table
-get_site_ornl_urls <- function(site_code_list) {
+get_ornl_site_url_list <- function(site_code_list) {
     status_table_url <- "https://fluxnet.ornl.gov/site_status"
 
     page_html <- read_html(status_table_url)
 
     ornl_url_list <- list()
 
-    for (sc in site_code_list) {
+    for (site_code in site_code_list) {
         # looks for table cell with site code as contents, then looks up the parent
         # row, and finds the href of the first link.
         xpath =  paste0("//td[text()='", site_code, "']/..")
@@ -132,7 +132,7 @@ get_site_ornl_urls <- function(site_code_list) {
             html_node("a") %>%
             html_attr("href")
 
-        ornl_url_list[[sc]] <- paste0("https://fluxnet.ornl.gov/", ornl_rel_url)
+        ornl_url_list[[site_code]] <- paste0("https://fluxnet.ornl.gov/", ornl_rel_url)
     }
 
     return(ornl_url_list)
@@ -142,11 +142,13 @@ get_site_ornl_urls <- function(site_code_list) {
 #' Get metadata from ORNL
 #'
 #' @return metadata list
-get_site_metadata_ornl <- function(metadata) {
+get_ornl_site_metadata <- function(metadata, site_url=NULL) {
     site_code <- get_site_code(metadata)
 
-    site_url <- get_site_ornl_url(site_code)
-    metadata$ORNL_URL <- site_url
+    if (is.null(site_url)) {
+        site_url <- get_site_ornl_url(site_code)
+        metadata$ORNL_URL <- site_url
+    }
 
     message("Trying to load metadata for ", site_code, " from ORNL (", site_url, ")")
 
@@ -166,9 +168,14 @@ get_site_metadata_ornl <- function(metadata) {
     metadata$SiteLongitude <- as.numeric(lat_lon[1])
 
     # Site Characteristics
-    table <- page_html %>% html_node("table#fluxnet_site_characteristics") %>% html_table()
-    metadata$SiteElevation <- table[table[1] == "GTOPO30 Elevation:"][2]
-    metadata$IGBP_vegetation_long <- table[table[1] == "IGBP Land Cover:"][2]
+    tryCatch({
+        table <- page_html %>% html_node("table#fluxnet_site_characteristics") %>% html_table()
+        metadata$SiteElevation <- table[table[1] == "GTOPO30 Elevation:"][2]
+        metadata$IGBP_vegetation_long <- table[table[1] == "IGBP Land Cover:"][2]
+    }, error = function(cond) {
+        message(site_code, " doesn't have a Site Characteristics table at ", site_url)
+    })
+
     # ORNL doesn't have any of these:
     #    metadata$IGBP_vegetation_short = NULL,
     #    metadata$TowerHeight = NaN,
@@ -188,7 +195,7 @@ get_site_metadata_ornl <- function(metadata) {
 #'
 #' @return metadata list
 get_site_metadata_web <- function(metadata) {
-    metadata <- get_site_metadata_ornl(metadata)
+    metadata <- get_ornl_site_metadata(metadata)
 
     # TODO: Add loaders for OzFlux, AmeriFlux, etc.
 
