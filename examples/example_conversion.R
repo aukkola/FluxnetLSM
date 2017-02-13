b4cf322e183d4bd66be4848241aebb3691422346
aukkola <anna.ukkola@students.mq.edu.au>
2017-02-13 17:18:14 +1100

    editing example file for a complete example

--- examples/example_conversion.R
+++ examples/example_conversion.R
@@ -9,9 +9,12 @@ library(FluxnetProcessing)  # convert_fluxnet_to_netcdf
 rm(list=ls(all=TRUE))
 
 
-#------------------------------
-# Set paths and options
+###################################
+###--- Set paths and options ---###
+###################################
 
+
+###--- Required inputs ---###
 
 # This directory should contain appropriate data from http://fluxnet.fluxdata.org/data/fluxnet2015-dataset/
 in_path <- "~/Documents/FLUXNET2016_processing/"
@@ -26,7 +29,10 @@ site_code <- "AU-How"
 fname  <- sprintf("FLX_%s_FLUXNET2015_FULLSET_HH_2001-2014_1-3.csv", site_code)
 infile <- paste(in_path, fname, sep="")
 
-# ERAinterim meteo file for gap-filling (set to FALSE if not desired)
+
+###--- Optional settings ---###
+
+# ERAinterim meteo file for gap-filling met data (set to FALSE if not desired)
 ERA_gapfill  <- TRUE
 fname        <- sprintf("FLX_%s_FLUXNET2015_ERAI_HH_1989-2014_1-3.csv", site_code)
 ERA_file     <- paste(in_path, fname, sep="")
@@ -35,23 +41,23 @@ ERA_file     <- paste(in_path, fname, sep="")
 datasetname    <- "Fluxnet2015"
 datasetversion <- "Nov16"
 
-
 #How many percent of time steps allowed to be missing in any given year?
 #and minimum number of consecutive years to process
 gap_threshold <- 20
 min_yrs       <- 2
 
-#Code produces plots to visualise outputs (set to NA if not desired)
-#Annual: average monthly cycle
-#Diurnal: average diurnal cycle by season
-#Timeseries: 14-day running mean time series
+#Should code produce plots to visualise outputs? (set to NA if not desired)
+#(annual: average monthly cycle; diurnal: average diurnal cycle by season;
+#timeseries: 14-day running mean time series)
 plot <- c("annual", "diurnal","timeseries")
 
 
-#------------------------------
-#Run analysis
 
-convert_fluxnet_to_netcdf(infile=infile, site_code=site_code, out_path=out_path, lib_path="./R",
+##########################
+###--- Run analysis ---###
+##########################
+
+convert_fluxnet_to_netcdf(infile=infile, site_code=site_code, out_path=out_path,
                           ERA_file=ERA_file, ERA_gapfill=ERA_gapfill, datasetname=datasetname, 
                           datasetversion=datasetversion, gap_threshold=gap_threshold, 
                           min_yrs=min_yrs, plot=plot)
