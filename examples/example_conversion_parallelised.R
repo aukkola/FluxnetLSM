f601a12b692de6b5fd0c0243e3a58ffab0afb42a
aukkola <anna.ukkola@students.mq.edu.au>
2017-02-14 13:39:13 +1100

    parallel example, seems to work now

--- examples/example_conversion_parallelised.R
+++ examples/example_conversion_parallelised.R
@@ -11,28 +11,35 @@ library(parallel)
 rm(list=ls(all=TRUE))
 
 
-#------------------------------
-# Set paths and options
+###################################
+###--- Set paths and options ---###
+###################################
 
+
+###--- Required inputs ---###
 
 # This directory should contain appropriate data from http://fluxnet.fluxdata.org/data/fluxnet2015-dataset/
-in_path <- "~/Documents/FLUXNET2016_processing/"
+in_path <- "~/Documents/FLUXNET2016_processing/Inputs/"
 
 #Outputs will be saved to this directory
-out_path <- "~/Documents/FLUXNET2016_processing/"
+out_path <- "~/Documents/FLUXNET2016_processing/Outputs/"
 
-#Fluxnet site IDs
+#Fluxnet site ID
 site_code <- c("AU-How", "FI-Hyy")
 
-# Fluxnet file (using FULLSET in this example)
-fname  <- sprintf("FLX_%s_FLUXNET2015_FULLSET_HH_2001-2014_1-3.csv", site_code)
-infile <- paste(in_path, fname, sep="")
+# Find Fluxnet files
+fname  <- sprintf("FLX_%s_FLUXNET2015_FULLSET", site_code)
+infile <- sapply(fname, function(x) list.files(in_path, pattern=x,
+                                               full.names=TRUE))
+  
 
+###--- Optional settings ---###
 
-# ERAinterim meteo file for gap-filling (set to FALSE if not desired)
+# ERAinterim meteo file for gap-filling met data (set to FALSE if not desired)
 ERA_gapfill  <- TRUE
-fname        <- sprintf("FLX_%s_FLUXNET2015_ERAI_HH_1989-2014_1-3.csv", site_code)
-ERA_file     <- paste(in_path, fname, sep="")
+eraname      <- sprintf("FLX_%s_FLUXNET2015_ERAI", site_code)
+ERA_file     <- sapply(eraname, function(x) list.files(in_path, pattern=x,
+                                                       full.names=TRUE))
 
 #Name and version of dataset being processed
 datasetname    <- "Fluxnet2015"
@@ -43,32 +50,37 @@ datasetversion <- "Nov16"
 gap_threshold <- 20
 min_yrs       <- 2
 
-#Code produces plots to visualise outputs (set to NA if not desired)
-#Annual: average monthly cycle
-#Diurnal: average diurnal cycle by season
-#Timeseries: 14-day running mean time series
+#Should code produce plots to visualise outputs? (set to NA if not desired)
+#(annual: average monthly cycle; diurnal: average diurnal cycle by season;
+#timeseries: 14-day running mean time series)
 plot <- c("annual", "diurnal","timeseries")
 
 
 
-#------------------------------
-#Run analysis
+##########################
+###--- Run analysis ---###
+##########################
 
 #Initialise clusters (using 2 cores here)
 cl <- makeCluster(getOption('cl.cores', 2))
-clusterExport(cl, 'convert_fluxnet_to_netcdf')
+
+#Import variables to cluster
+clusterExport(cl, 'out_path')
+if(exists("gap_threshold"))  {clusterExport(cl, 'gap_threshold')}
+if(exists("min_yrs"))        {clusterExport(cl, 'min_yrs')}
+if(exists("plot"))           {clusterExport(cl, 'plot')}
+if(exists("ERA_gapfill"))    {clusterExport(cl, 'ERA_gapfill')}
+if(exists("datasetname"))    {clusterExport(cl, 'datasetname')}
+if(exists("datasetversion")) {clusterExport(cl, 'datasetversion')}
 
 
-clusterMap(cl=cl, function(x,y,z) convert_fluxnet_to_netcdf(infile=x, site_code=y, ERA_file=z, 
-                                                            out_path=out_path, ERA_gapfill=ERA_gapfill,
-                                                            datasetname=datasetname, datasetversion=datasetversion,
-                                                            gap_threshold=gap_threshold, min_yrs=min_yrs, plot=plot),
-                                                            x=infile, y=site_code, z=ERA_file)
+clusterMap(cl=cl, function(x,y,z) {library(FluxnetProcessing) 
+                                   convert_fluxnet_to_netcdf(infile=x, site_code=y, ERA_file=z, lib_path="./R",
+                                   out_path=out_path, ERA_gapfill=ERA_gapfill,
+                                   datasetname=datasetname, datasetversion=datasetversion,
+                                   gap_threshold=gap_threshold, min_yrs=min_yrs, plot=plot)
+                                   }, x=infile, y=site_code, z=ERA_file)
 
 
 stopCluster(cl)
-
-
-
-
 
