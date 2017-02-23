c869cb04f6e75ee2f0634f0daf6a8e07e32d609b
mdekauwe <mdekauwe@gmail.com>
2017-02-23 15:08:18 +1100

    Fixed spaces

--- R/Timing_general.R
+++ R/Timing_general.R
@@ -5,22 +5,18 @@
 #' TODO: Check and merge back in to palsR
 #'
 #' @export
-#' 
+#'
 findStartTime <- function(start){
-  
+
   # Find starting date / time:
   sday   <- as.numeric(format(start, format="%d"))
   smonth <- as.numeric(format(start, format="%m"))
   syear  <- as.numeric(format(start, format="%Y"))
-  
   shod   <- as.numeric(format(start, format="%H")) # starting hour of day
-  
+
   # Collate start time variables:
   starttime <- list(syear=syear,smonth=smonth,sday=sday,shod=shod)
-  
-  
+
   return(starttime)
 
 }
-
-
