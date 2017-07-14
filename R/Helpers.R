3244e42c48127ace5771f7b4ae6dfe70d231305a
aukkola <amukkola@gmail.com>
2017-07-14 11:52:54 +1000

    adding new functoin to retrieve la thuile data policy

--- R/Helpers.R
+++ R/Helpers.R
@@ -5,23 +5,30 @@ get_fluxnet_file_template <- function(site_code = "[A-Z]{2}-[A-Za-z0-9]{3}",
                                       datasetname = "FLUXNET2015",
                                       subset = "FULLSET",
                                       resolution = "[A-Z]{2}",
-                                      years = "[0-9]{4}-[0-9]{4}",
+                                      flx2015_years = "[0-9]{4}-[0-9]{4}",
+                                      lathuile_year="[0-9]{4}",
                                       datasetversion = "[0-9]{1}-[0-9]{1}",
                                       extension=".csv") {
-    version <- gsub("\\.", "-", datasetversion)
-    if (is.character(resolution) & nchar(resolution) > 0) {
+  
+    if(datasetname=="LaThuile"){
+      file_template <- paste(site_code, lathuile_year, sep=".")
+    } else{
+      version <- gsub("\\.", "-", datasetversion)
+      if (is.character(resolution) & nchar(resolution) > 0) {
         file_template <- paste("FLX", site_code, datasetname, subset, resolution,
-                               years, version, sep = "_")
+                               flx2015_years, version, sep = "_")
         file_template <- paste0(file_template, extension)
-    } else {
-        file_template <- paste("FLX", site_code, datasetname, subset, years,
+      } else {
+        file_template <- paste("FLX", site_code, datasetname, subset, flx2015_years,
                                version, sep = "_")
         file_template <- paste0(file_template, extension)
+      }      
     }
 
     return(file_template)
 }
 
+#-----------------------------------------------------------------------------
 
 #' Gets a Fluxnet ERA interim file template (or full file name if site codes and years are specified)
 #'
@@ -29,6 +36,8 @@ get_fluxnet_file_template <- function(site_code = "[A-Z]{2}-[A-Za-z0-9]{3}",
 get_fluxnet_erai_template <- function(site_code, ...) {
     return(get_fluxnet_file_template(site_code = site_code, subset = "ERAI", ...))
 }
+
+#-----------------------------------------------------------------------------
 
 #' Gets Fluxnet file(s) available at a given path
 #'
@@ -40,6 +49,7 @@ get_fluxnet_files <- function(path, site_code = "[A-Z]{2}-[A-Za-z0-9]{3}", ...) 
     return(files)
 }
 
+#-----------------------------------------------------------------------------
 
 #' Gets Fluxnet ERA Interim file(s) available at a given path
 #'
@@ -51,6 +61,7 @@ get_fluxnet_erai_files <- function(path, site_code = "[A-Z]{2}-[A-Za-z0-9]{3}", 
   return(files)
 }
 
+#-----------------------------------------------------------------------------
 
 #' Gets Fluxnet dataset version from Fluxnet file
 #'
@@ -61,6 +72,7 @@ get_fluxnet_version_no <- function(file) {
   return(version)
 }
 
+#-----------------------------------------------------------------------------
 
 #' Gets Fluxnet site_code from Fluxnet file.
 #' Useful when processing multiple files
@@ -68,10 +80,10 @@ get_fluxnet_version_no <- function(file) {
 get_path_site_code <- function(path) {
   #assumes a FLUXNET2015 file, e.g FLX_US-Ha1_FLUXNET2015_FULLSET_HR_1991-2012_1-3.csv
   filename  <- basename(path)
-  if (startsWith(filename, "FLX_")) {
+  if (substring(filename, 1, nchar("FLX")) == "FLX"){     #better implementation but only for R>3.3: startsWith(filename, "FLX_")) {
     site_code <- substr(filename, start = 5, stop = 10)
   } else {
-    site_code <- substr(filename, stop = 5)
+    site_code <- substr(filename, start=1, stop = 6)
   }
   if (!grepl("^[A-Z]{2}-[A-Za-z0-9]{3}$", site_code)) {
     stop("Site code not found in file name")
@@ -79,3 +91,21 @@ get_path_site_code <- function(path) {
 
   return(site_code)
 }
+
+#-----------------------------------------------------------------------------
+
+#' Gets La Thuile data policy for each site year
+#' @export
+get_lathuile_datapolicy <- function(site_code, site_use){
+  
+  #Read data policy file (must use check.names so R doesn't add "X" in front of colnames)
+  use_policy <- read.csv(sites_use, header=TRUE, check.names=FALSE)
+  
+  #Extract site
+  site_policy <- use_policy[use_policy$site==site_code,]
+
+  return(site_policy)
+}
+
+
+
