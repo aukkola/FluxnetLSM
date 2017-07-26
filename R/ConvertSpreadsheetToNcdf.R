8c059b65fbb0276028833afc277d31cfbf467066
aukkola <amukkola@gmail.com>
2017-07-26 16:50:16 +1000

    changing output file naming

--- R/ConvertSpreadsheetToNcdf.R
+++ R/ConvertSpreadsheetToNcdf.R
@@ -447,20 +447,11 @@ convert_fluxnet_to_netcdf <- function(site_code, infile, era_file=NA, out_path,
     
     
     ### Create output file names ###
-    
-    #If only one year, only write start year, else write time period
-    if(start_yr[k]==end_yr[k]){
-      metfilename  <- paste(out_paths$nc, "/", site_code, "_", start_yr[k], 
-                            "_", conv_opts$datasetname, "_Met.nc", sep="")
-      fluxfilename <- paste(out_paths$nc, "/", site_code, "_", start_yr[k], 
-                            "_", conv_opts$datasetname, "_Flux.nc", sep="")
-      
-    } else {
-      metfilename  <- paste(out_paths$nc, "/", site_code, "_", start_yr[k], 
-                            "-", end_yr[k], "_", conv_opts$datasetname, "_Met.nc", sep="")
-      fluxfilename <- paste(out_paths$nc, "/", site_code, "_", start_yr[k], 
-                            "-", end_yr[k], "_", conv_opts$datasetname, "_Flux.nc", sep="")
-    }
+    metfilename  <- paste(out_paths$nc, "/", site_code, "_", start_yr[k], 
+                          "-", end_yr[k], "_", conv_opts$datasetname, "_Met.nc", sep="")
+    fluxfilename <- paste(out_paths$nc, "/", site_code, "_", start_yr[k], 
+                          "-", end_yr[k], "_", conv_opts$datasetname, "_Flux.nc", sep="")
+
     
     #Save file names
     met_files[k]  <- metfilename
