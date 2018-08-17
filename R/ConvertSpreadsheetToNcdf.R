c2a5ed45529cedcdfeb6f9fd43dabe6f758911f6
aukkola <amukkola@gmail.com>
2018-08-17 15:02:48 +1000

    also need to rename data frame headers

--- R/ConvertSpreadsheetToNcdf.R
+++ R/ConvertSpreadsheetToNcdf.R
@@ -224,13 +224,20 @@ convert_fluxnet_to_netcdf <- function(site_code, infile, era_file=NA, out_path,
     
     if(any(DataFromText$vars == "NEE_GPP_qcOK")) {
       
+      #Change variable name and column header in data frame
       #NEE flag
       ind_nee <- which(DataFromText$out_vars == "NEE_qc")
-      if(length(ind_nee) > 0) { DataFromText$vars[ind_nee] <- "NEE_fqc"}
+      if(length(ind_nee) > 0) { 
+        DataFromText$vars[ind_nee] <- "NEE_fqc"
+        colnames(DataFromText$data)[ind_nee] <- "NEE_fqc"
+        }
       
       #GPP flag
       ind_gpp <- which(DataFromText$out_vars == "GPP_qc")
-      if(length(ind_nee) > 0) { DataFromText$vars[ind_gpp] <- "GPP_fqc"}
+      if(length(ind_nee) > 0) { 
+        DataFromText$vars[ind_gpp] <- "GPP_fqc"
+        colnames(DataFromText$data)[ind_gpp] <- "NEE_fqc"
+      }
       
     }
   }
