e0b96b37db85cf0f93c2a54f538f1137bcbdc262
aukkola <amukkola@gmail.com>
2018-09-07 10:39:11 +1000

    adding browser

--- R/Conversions.R
+++ R/Conversions.R
@@ -90,9 +90,10 @@ ChangeUnits <- function(datain, varnames, site_log){
       ## Specific humidity from VPD (in kg/kg, calculate from tair, VPD and psurf)
       } else if(datain$vars[k] %in% varnames$vpd & flx_units[k]=="hPa" & alma_units[k]=="kg/kg"){  
         
+        browser()
         #Find Tair and PSurf units
         psurf_units <- flx_units[names(flx_units) %in% varnames$airpressure]
-        tair_units  <- flx_units[names(flx_units) %in% varnames$tair]
+        tair_units  <- flx_units[names(flx_units) %in% varnames$tair
         
         #If already converted, reset units to new converted units
         if(converted[which(datain$vars %in% varnames$airpressure)]) {
