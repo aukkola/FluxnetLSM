8fc0390204bb2736f32a8fe4f8825a7810c03c9a
aukkola <amukkola@gmail.com>
2018-09-07 10:17:31 +1000

    checking if new outvars now work

--- R/ConvertSpreadsheetToNcdf.R
+++ R/ConvertSpreadsheetToNcdf.R
@@ -488,6 +488,7 @@ convert_fluxnet_to_netcdf <- function(site_code, infile, era_file=NA, out_path,
   ###--- Write output met and flux NetCDF files ---###
   ####################################################
   
+  browser()
   #Set all NA values to Nc missing value
   DataFromText$data[is.na(DataFromText$data)] <- Nc_MissingVal
 
