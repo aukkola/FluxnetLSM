7de2118abdeecf8490d732a85a09e6d933576c9a
Anna Ukkola <a.ukkola@unsw.edu.au>
2023-05-02 16:11:38 +1000

    hitting an error reading metadata from fluxdata for some ICOS sites, adding error catching

--- R/Site_metadata.R
+++ R/Site_metadata.R
@@ -463,7 +463,7 @@ get_fluxdata_org_site_metadata <- function(metadata, site_url=NULL) {
 
     message("Trying to load new_metadata for ", site_code, " from Fluxdata.org (", site_url, ")")
 
-    page_html <- read_html(site_url)
+    page_html <- tryCatch(read_html(site_url), error = function(e) NULL)
 
     # General info
     table_data <- tryCatch(page_html %>% html_node("table.maininfo") %>% html_table(),
